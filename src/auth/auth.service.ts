e48227b63d651add21ec05bfdfc48c1b151495dd
cpvl <2fc.data@gmail.com>
2026-05-19 12:08:46 -0300

    ajuste admin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../models';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,13 +17,16 @@ export class AuthService {
     username: string,
     password: string,
   ): Promise<Partial<User> | null> {
+    this.logger.log(`Validating user: ${username}`);
     const user = await this.usersService.findUser({ username });
 
     if (!user) {
+      this.logger.warn(`User not found: ${username}`);
       return null;
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
+    this.logger.log(`Password valid for ${username}: ${isPasswordValid}`);
 
     if (isPasswordValid) {
       const { id, username: uname, role } = user;
