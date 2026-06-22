69e1e544845df25e79f4fe636e108a7ac0ada8ae
cpvl <2fc.data@gmail.com>
2026-06-22 17:57:26 -0300

    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -9,26 +9,28 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '5mb' }));
   app.use(urlencoded({ limit: '5mb', extended: true }));
-  app.enableCors({
-    credentials: true,
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'https://powderblue-crab-369130.hostingersite.com',
-        /^http:\/\/localhost:\d+$/,
-      ];
+  app.enableCors(
+    // {
+    //   credentials: true,
+    //   origin: (origin, callback) => {
+    //     const allowedOrigins = [
+    //       'https://powderblue-crab-369130.hostingersite.com',
+    //       /^http:\/\/localhost:\d+$/,
+    //     ];
 
-      if (
-        !origin ||
-        allowedOrigins.some((pattern) =>
-          pattern instanceof RegExp ? pattern.test(origin) : pattern === origin,
-        )
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  });
+    //     if (
+    //       !origin ||
+    //       allowedOrigins.some((pattern) =>
+    //         pattern instanceof RegExp ? pattern.test(origin) : pattern === origin,
+    //       )
+    //     ) {
+    //       callback(null, true);
+    //     } else {
+    //       callback(new Error('Not allowed by CORS'));
+    //     }
+    //   },
+    // }
+  );
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT || 8000);
